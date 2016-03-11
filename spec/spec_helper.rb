62cb097c1bb074f19b9c3e226078905fbadb0a62
Pooyan Khosravi <pekhee@gmail.com>
2016-03-11 12:14:41 +0800

    correct name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
 require "bundler/setup"
+require "json"
 
 if ENV["CI"]
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
 end
 
-require "rspec_matchers_adhere_to_schema"
+require "rspec/matchers/adhere_to_schema"
 
 Dir[File.join(File.dirname(__FILE__), "support/extensions/**/*.rb")].each { |file| require file }
 Dir[File.join(File.dirname(__FILE__), "support/kit/**/*.rb")].each { |file| require file }
